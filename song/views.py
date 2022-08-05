4126eb31e10e6cc890477a13639bcf4d7bfb500e
Fuller1212 <fullermorrison@gmail.com>
2022-08-05 13:12:36 -0500

    Completed Post function in Views.py

--- song/views.py
+++ song/views.py
@@ -1,5 +1,20 @@
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .models import Song
+from .serializers import SongSerializer
 
 @api_view(['GET'])
 def song_list(request):
+    if request.method == 'GET':
+        song = Song.objects.all()
+        serializer = SongSerializer(song, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = SongSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+           serializer.save()
+           return Response(serializer.data) 
+        else:
+            return Response(serializer, status=status.HTTP_201_CREATED)
